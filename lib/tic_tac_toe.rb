4dcd112f6a58a76693fc4385a4aa390f9a6aa9f1
Grant Raisig <gfraisig@outlook.com>
2019-04-16 13:57:52 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -73,17 +73,7 @@ def play(board)
   
 end
 
-def turn_count(board)
 
-  count = 0
-  board.each do |cell|
-    if cell == "X" || cell == "O"
-      count += 1
-    end
-  end
-  
-  return count
-end
 
 def current_player(board)
   
