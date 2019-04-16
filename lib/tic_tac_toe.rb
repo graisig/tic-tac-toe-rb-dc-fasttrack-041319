addf4f57fc6c12fc5ca4648d9e7677b77640732d
Grant Raisig <gfraisig@outlook.com>
2019-04-16 14:05:51 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -138,7 +138,15 @@ def play(board)
   end
   
   
-  
+  until the game is over
+    take turns
+  end
+   
+  if the game was won
+    congratulate the winner
+  else if the game was a draw
+    tell the players it has been a draw
+  end 
   
   
   
