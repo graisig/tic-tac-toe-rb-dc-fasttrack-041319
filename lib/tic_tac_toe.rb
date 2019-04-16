04e5fa4f3726b393f511344f37f7229dd468b5a5
Grant Raisig <gfraisig@outlook.com>
2019-04-16 14:01:01 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -130,12 +130,7 @@ end
 
 
 
-def full?(board)
 
-  full_board = [0,1,2,3,4,5,6,7,8].all? {|index| position_taken?(board, index)}
-  return full_board
-
-end
 
 def draw?(board)
   return full?(board) && !won?(board)
